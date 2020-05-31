c098b33ddb5f3d309e80f63358be1eaa3cd5eb70
Rekha <saipadmarekha@gmail.com>
2020-05-31 16:00:46 +0530

    Initial commit

---
+++ area.py
@@ -0,0 +1,12 @@
+a=int(input("enter the length="))
+b=int(input("enter the breadth="))
+as1=0
+sr=0
+c=0.0
+as1=a*a
+sr=a*b
+r=int(input("enter radius="))
+c=3.14*r*r
+print("Area of square=",as1)
+print("Area of rectangle=",sr)
+print("Area of circle=",c)
