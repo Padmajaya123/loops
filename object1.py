87852e82bf4d787dda573ce972bf15d05445c3d4
Rekha <saipadmarekha@gmail.com>
2020-06-01 21:00:24 +0530

    Initial commit

---
+++ object1.py
@@ -0,0 +1,10 @@
+class book:
+    v1="william"
+    v2="henry"
+    def func(self):
+        print("I like ",self.v1," books")
+        print("I like ",self.v2," books")
+
+r=book()
+print(r.v1)
+r.func()
