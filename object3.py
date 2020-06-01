3ec4a278d95b3182a0cf1448d6e548c24ecf71ab
Rekha <saipadmarekha@gmail.com>
2020-06-01 21:09:50 +0530

    Initial commit

---
+++ object3.py
@@ -0,0 +1,15 @@
+class study:
+    sub="Maths"
+    def __init__(self,s1,s2):
+        self.s1=s1
+        self.s2=s2
+r1=study("English","Computer")
+r2=study("Physics","Chemistry")
+print("subject details")
+print("main subject=",r1.sub)
+print("first subject=",r1.s1)
+print("second subject=",r1.s2)
+print("subject details")
+print("main subject=",r2.sub)
+print("third subject=",r2.s1)
+print("fourth subject=",r2.s2)
