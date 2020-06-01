2a3afd80a15a1aa90215888bdd7705e47d5827ea
Rekha <saipadmarekha@gmail.com>
2020-06-01 17:34:18 +0530

    Initial commit

---
+++ count.py
@@ -0,0 +1,4 @@
+from array import *
+array1= array('i', [1,9,3,5,3,7,9,3,9,9])
+print("Original array: "+str(array1))
+print("Number of occurrences of the number 9 in the said array: "+str(array1.count(9)))
