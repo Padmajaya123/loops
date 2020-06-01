9fe78bed5436c23acddd2d072c4adb17f664fe9d
Rekha <saipadmarekha@gmail.com>
2020-06-01 21:26:12 +0530

    Initial commit

---
+++ object6.py
@@ -0,0 +1,25 @@
+class Parent:
+   parentAttr = 100
+   def __init__(self):
+      print("Calling parent constructor")
+
+   def parentMethod(self):
+      print ('Calling parent method')
+
+   def setAttr(self, attr):
+      Parent.parentAttr = attr
+
+   def getAttr(self):
+      print("Parent attribute :", Parent.parentAttr)
+
+class Child(Parent):
+   def __init__(self):
+      print("Calling child constructor")
+
+   def childMethod(self):
+      print('Calling child method')
+r = Child()
+r.childMethod()
+r.parentMethod()
+r.setAttr(600)
+r.getAttr()
